8fdad7346c076ecbb9876b6d77b45aacd3a35dd5
Ferdinand Calo <fercalo96@yahoo.de>
2023-04-03 22:33:45 +0200

    Create ItemManager.java

---
+++ src/main/java/net/fexcraft/mod/fsmm/util/ItemManager.java
@@ -0,0 +1,19 @@
+package net.fexcraft.mod.fsmm.util;
+
+import net.minecraft.commands.CommandSource;
+import net.minecraft.world.entity.player.Player;
+
+public class ItemManager {
+
+    public static long countInInventory(CommandSource log){
+        return 0;
+    }
+
+    public static void removeFromInventory(Player player, long total){
+
+    }
+
+    public static void addToInventory(Player player, long l){
+
+    }
+}
